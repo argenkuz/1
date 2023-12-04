71b8ced3381b05400a29d65dee671041fb40966d
unknown <argenkulzanov@gmail.com>
2023-12-05 03:09:07 +0600

    1

--- martians.py
+++ martians.py
@@ -17,9 +17,4 @@ while game:
     summ = 0
     for _ in range(3):
         summ += killometres[int_validation("select killometr: ") - 1]
-    if summ == 713:
-        print("You've won!")
-        game = False
-    else:
-        print("wrong! Tentacles changed position of boxes.Try again")
-        random.shuffle(killometres)
+
